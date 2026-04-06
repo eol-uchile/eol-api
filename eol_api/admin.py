3f99801f7a0ac76fbe030dacf4c681334e6e7777
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-04-06 17:53:19 -0400

    refactor: change how ´created´ is generated in ClientCourseAccess

--- eol_api/admin.py
+++ eol_api/admin.py
@@ -9,4 +9,20 @@ class ClientCourseAccessAdmin(admin.ModelAdmin):
     list_display = ('client', 'course_id', 'created','created_by',)
     search_fields = ['course_id', 'created',]
 
+    # save user when is created
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_exclude(self, request, obj=None):
+        if obj is None:
+            return ('created_by',)
+        return ()
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('created_by',)
+        return ()
+
 admin.site.register(ClientCourseAccess, ClientCourseAccessAdmin)
