1db2845060cf052f40947ad1a9746638f146d6a7
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-04-07 17:25:21 -0400

    refactor: update ClientCourseAccess model adding history
    - update `created_by` removing its unique constraint.
    - add history to model.
    - add migration script.

--- eol_api/models.py
+++ eol_api/models.py
@@ -1,9 +1,10 @@
 # Python Standard Libraries
-import datetime
+import uuid
 
 # Installed packages (via pip)
 from django.contrib.auth.models import User
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 # Edx dependencies
 from oauth2_provider.models import Application
@@ -11,6 +12,14 @@ from opaque_keys.edx.django.models import CourseKeyField
 
 class ClientCourseAccess(models.Model):
     client = models.ForeignKey(Application, on_delete=models.CASCADE)
-    course_id = course_id = CourseKeyField(max_length=255, db_index=True, unique=True,verbose_name=('course'))
+    course_id = CourseKeyField(max_length=255, db_index=True, verbose_name=('course'))
     created = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    # An audit row will be created for every change to a ClientCourseAccess. This
+    # will create a new model behind the scenes - HistoricalClientCourseAccess and a
+    # table named 'eol_api_clientcourseaccess_history'.
+    history = HistoricalRecords(
+        history_id_field=models.UUIDField(default=uuid.uuid4),
+        table_name='eol_api_clientcourseaccess_history'
+    )
